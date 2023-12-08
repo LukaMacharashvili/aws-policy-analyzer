f5a1ae9c345f2b988ee73329a97e3b3048849689
Luka Macharashvili <macharashvililuka12@gmail.com>
2023-12-08 17:53:16 +0400

    permissions checker fix

--- Main.hs
+++ Main.hs
@@ -1,93 +1,94 @@
 {-# HLINT ignore "Use lambda-case" #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
+import Control.Applicative
+import Data.Maybe (fromMaybe)
 import Parser
 
-data Effect = Allow | Deny
-  deriving (Show, Eq)
-
-newtype Action = Action Text
-  deriving (Show, Eq)
-
-newtype Resource = Resource Text
-  deriving (Show, Eq)
-
 data Statement = Statement
-  { effect :: Effect,
-    action :: Action,
-    resource :: Resource
+  { effect :: String,
+    action :: String,
+    resource :: String
   }
   deriving (Show, Eq)
 
 data Policy = Policy
-  { version :: Text,
+  { version :: String,
     statements :: [Statement]
   }
   deriving (Show, Eq)
 
-instance FromJSON Effect where
-  parseJSON = withText "Effect" $ \t ->
-    case t of
-      "Allow" -> return Allow
-      "Deny" -> return Deny
-      _ -> fail "Invalid effect"
-
-instance FromJSON Action where
-  parseJSON = withText "Action" $ \t -> return $ Action t
-
-instance FromJSON Resource where
-  parseJSON = withText "Resource" $ \t -> return $ Resource t
-
-instance FromJSON Statement where
-  parseJSON = withObject "Statement" $ \obj -> do
-    eff <- obj .: "Effect"
-    act <- obj .: "Action"
-    res <- obj .: "Resource"
-    return $ Statement eff act res
-
-instance FromJSON Policy where
-  parseJSON = withObject "Policy" $ \obj -> do
-    ver <- obj .: "Version"
-    sts <- obj .: "Statement"
-    return $ Policy ver sts
-
 validateStatement :: Statement -> Bool
 validateStatement stmt =
   case (effect stmt, action stmt, resource stmt) of
-    (Allow, Action a, Resource r) -> not (Data.Text.null a) && not (Data.Text.null r)
-    (Deny, Action a, Resource r) -> not (Data.Text.null a) && not (Data.Text.null r)
+    (e, a, r) -> not (null e) && not (null a) && not (null r)
 
 validatePolicy :: Policy -> Bool
-validatePolicy (Policy version stmts) = all validateStatement stmts && not (Data.Text.null version)
+validatePolicy (Policy version stmts) = all validateStatement stmts && not (null version)
 
-explicitDeny :: Policy -> Bool
-explicitDeny (Policy _ stmts) =
+explicitDeny :: Policy -> String -> String -> Bool
+explicitDeny (Policy _ stmts) a r =
   any
     ( \stmt ->
         case (effect stmt, action stmt, resource stmt) of
-          (Deny, Action a, Resource r) -> a == a && r == r
+          ("Deny", a', r') | a == a' && r == r' -> True
           _ -> False
     )
     stmts
 
-explicitAllow :: Policy -> Bool
-explicitAllow (Policy _ stmts) =
+explicitAllow :: Policy -> String -> String -> Bool
+explicitAllow (Policy _ stmts) a r =
   any
     ( \stmt ->
         case (effect stmt, action stmt, resource stmt) of
-          (Allow, Action a, Resource r) -> a == a && r == r
+          ("Allow", a', r') | a == a' && r == r' -> True
           _ -> False
     )
     stmts
 
-hasPermission :: Policy -> Action -> Resource -> Bool
-hasPermission (Policy version stmts) (Action a) (Resource r) = res
-  where
-    deny = explicitDeny (Policy version stmts)
-    allow = explicitAllow (Policy version stmts)
-    res = not deny && allow
+hasPermission :: Maybe Policy -> String -> String -> Bool
+hasPermission Nothing _ _ = False
+hasPermission (Just policy) a r = not (explicitDeny policy a r) && explicitAllow policy a r
+
+extractValueFromKeyValPairs :: String -> [(String, JsonValue)] -> Maybe JsonValue
+extractValueFromKeyValPairs key [] = Nothing
+extractValueFromKeyValPairs key ((k, v) : xs) =
+  if k == key
+    then Just v
+    else extractValueFromKeyValPairs key xs
+
+arrangeStatement :: [(String, JsonValue)] -> Maybe Statement
+arrangeStatement xs =
+  let va = extractValueFromKeyValPairs "Effect" xs
+      vb = extractValueFromKeyValPairs "Action" xs
+      vc = extractValueFromKeyValPairs "Resource" xs
+   in case (va, vb, vc) of
+        (Just (JsonString a), Just (JsonString b), Just (JsonString c)) -> Just (Statement a b c)
+        _ -> Nothing
+
+arrangePolicy :: String -> [JsonValue] -> Maybe Policy
+arrangePolicy version stmts = do
+  let stmts' = [stmt | JsonObject stmt <- stmts]
+  let stmts'' = [arrangeStatement stmt | stmt <- stmts']
+  let stmts''' = sequenceA stmts''
+
+  case stmts''' of
+    Just stmts'''' -> Just (Policy version stmts'''')
+    Nothing -> Nothing
 
 main :: IO ()
 main = do
   let json = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Action\":\"s3:ListBucket\",\"Resource\":\"arn:aws:s3:::examplebucket\"},{\"Effect\":\"Deny\",\"Action\":\"s3:ListBucket\",\"Resource\":\"arn:aws:s3:::examplebucket2\"}]}"
-  print $ runParser jsonValue json
+
+  let Just (whatsLeft, JsonObject xs) = runParser jsonValue json
+
+  let [(_, JsonString version)] = [(name, value) | (name, value) <- xs, name == "Version"]
+  let [(_, JsonArray stmts)] = [(name, value) | (name, value) <- xs, name == "Statement"]
+
+  let policy = arrangePolicy version stmts
+  let has1 = hasPermission policy "s3:ListBucket" "arn:aws:s3:::examplebucket"
+  let has2 = hasPermission policy "s3:ListBucket" "arn:aws:s3:::examplebucket2"
+
+  print policy
+  print has1
+  print has2
